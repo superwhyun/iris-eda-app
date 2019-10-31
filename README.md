6341f294dcf7807576c4750e67753fc97c7b3d6c
HYUN WOOK <whyun@etri.re.kr>
2019-11-01 04:21:27 +0900

    Iris EDA app deploy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iris-eda-app
 Simple Iris EDA app with Streamlit
+
+# Required Files
+1. setup.sh
+2. procfile
+3. requirements.txt
---
+++ iris_eda_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.title("Iris EDA App with streamlit")
+    st.subheader("Streamlit is cool")
+    st.subheader("WYSWYG")
+    st.subheader("I have to refresh the web browser")
+
+
+
+
+if __name__ == '__main__':
+    main()
